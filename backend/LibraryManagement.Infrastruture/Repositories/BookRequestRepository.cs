67d7ac248c7e0b7434961ad317cf8a860e68af10
Prithviraj Patil <153794336+Prithviraj6@users.noreply.github.com>
2026-05-24 16:19:48 +0530

    created repo directory

---
+++ backend/LibraryManagement.Infrastruture/Repositories/BookRequestRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagement.Infrastruture.Repositories
+{
+    internal class BookRequestRepository
+    {
+    }
+}
